8b06084b6640604543d543b685485466bd82febe
Alpha-loop <fxalpha36@gmail.com>
2025-06-29 02:00:58 -0700

    Update .env and index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 // Routes
 app.use('/api', quoteRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
